a801bb82f1d04b58120c7615d75cfa5dc63bb552
JuanOrozco213 <Juanorozco213@gmail.com>
2017-11-07 18:09:19 -0800

    Added SQL constraints
    Keys were added and new tables

--- WebContent/SQL/SQLtables.sql
+++ WebContent/SQL/SQLtables.sql
@@ -1,34 +1,46 @@
-drop table if exists Musician;
-drop table if exists Album;
-drop table if exists Instrument;
-drop table if exists Song;
+drop table if exists Musician CASCADE;
+drop table if exists Album CASCADE;
+drop table if exists Instrument CASCADE;
+drop table if exists Song CASCADE;
+drop table if exists Playedby CASCADE;
+drop table if exists performedby CASCADE;
 
 CREATE TABLE Musician (
-    ssn int,
-    name varchar(255),
-    phone int,
-   address varchar(255),
+    ssn int PRIMARY KEY ,
+    name varchar(255) NOT NULL,
+    phone int NOT NULL,
+    address varchar(255) NOT NULL,
     ifPoor boolean default true
 );
 
 CREATE TABLE Album (
-    id int,
-    albumId int,
-    pSSN int,
-    copyrightDate date,
-    title varchar(255),
-   format varchar(255)
+    id int PRIMARY KEY ,
+    albumId int NOT NULL,
+    pSSN int  REFERENCES Musician (ssn) ON DELETE CASCADE ON UPDATE CASCADE,
+    copyrightDate date NOT NULL,
+    title varchar(255) NOT NULL,
+    format varchar(255) NOT NULL
 );
 CREATE TABLE Instrument (
-    id int,
-   name varchar(255),
-   musicalKey varchar(255)
+    id int PRIMARY KEY  ,
+    name varchar(255) NOT NULL,
+    musicalKey varchar(255) NOT NULL
 );
 
 CREATE TABLE Song(
-    album_id int,
-    song_id int,
-    title varchar(255)
+    song_id int PRIMARY KEY,
+    album_id int REFERENCES Album (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    title varchar(255) NOT NULL
+);
+
+CREATE TABLE Playedby(
+    Mssn int  REFERENCES Musician (ssn) ON DELETE CASCADE ON UPDATE CASCADE,
+    Instrument_id int REFERENCES Instrument (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE performedby(
+    Mssn int  REFERENCES Musician (ssn) ON DELETE CASCADE ON UPDATE CASCADE,
+    song_id int REFERENCES Song (song_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
